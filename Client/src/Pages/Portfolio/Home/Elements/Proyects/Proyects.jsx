fc9ce456c3c33d29c2b87b468ce199d6728ee51d
xNatzhu <agustinnahuelsaravia@gmail.com>
2024-02-18 09:11:03 -0300

    keep

--- Client/src/Pages/Portfolio/Home/Elements/Proyects/Proyects.jsx
+++ Client/src/Pages/Portfolio/Home/Elements/Proyects/Proyects.jsx
@@ -1,7 +1,7 @@
 export default function Proyects() {
     return(
-        <>
+        <div className="bg-[#222434] px-[30px] w-[100%] flex h-[100vh]">
             <h1>Hola</h1>
-        </>
+<       /div>
     )
 }
